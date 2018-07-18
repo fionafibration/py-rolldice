f640f0a10739f4f3d2a044623065a0405ffef0f9
Fin <spamsuckersunited@gmail.com>
2018-07-18 16:51:12 -0600

    version numbers

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-rolldice",
-    version="0.3.5",
+    version="0.3.6",
     author="Finian Blackett",
     author_email="spamsuckersunited@gmail.com",
     description="A module for parsing dice notation",
